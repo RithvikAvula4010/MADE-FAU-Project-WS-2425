6e15ee5cf895ba6016c6ef1e18b87fcfa7c86aae
Prathamesh Agare <prathamesh.shankar.agare@gmail.com>
2024-12-02 19:52:05 +0100

    Update tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-python ../project/tests.py
+
+# Run pipeline.sh file
+bash ../project/pipeline.sh
+
+# Check if the previous command was successful
+if [ $? -eq 0 ]; then
+    echo "pipeline.sh ran successfully. Proceeding to run tests.py..."
+    
+    # Run the Python script
+    python ../project/tests.py
+else
+    echo "pipeline.sh encountered an error. Exiting script."
+    exit 1
+fi
