964378f333003d2a0a23dd1917b3a05fe8c0b24f
Prathamesh Agare <prathamesh.shankar.agare@gmail.com>
2024-12-03 19:28:18 +0100

    Update tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Run pipeline.sh file
+# pipeline was created in Jayvee. Data Extraction taking too much time to extract the data (approx 20 to 30 min), hence do not execute the pipeline.
+
 bash ../project/pipeline.sh
 
 # Check if the previous command was successful
