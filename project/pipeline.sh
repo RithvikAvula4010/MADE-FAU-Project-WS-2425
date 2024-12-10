b1defbed7b931356b627cd218672ecc01a6201f0
Prathamesh Agare <prathamesh.shankar.agare@gmail.com>
2024-12-10 18:04:07 +0100

    Update pipeline.sh

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Automated feed extraction is slow, so avoid running it via the JV file.
 #Data Extraction taking too much time to extract the data (approx 20 to 30 min), hence do not execute the pipeline.
-jayvee ../project/automated_pipeline.jv
+jayvee project/automated_pipeline.jv
